0ab1ecd75cabed33c85f0e0611b4e78eca9cfbf3
Philip W. Howard <phil.w.howard@gmail.com>
2020-04-22 17:50:54 -0700

    Can position and move handles.

--- HullRotate/HullWindow.xaml.cs
+++ HullRotate/HullWindow.xaml.cs
@@ -88,23 +88,117 @@ namespace HullRotate
 
                 m_hull.GetBulkheadPoints(bulkhead, points);
 
-                m_handle = new Button[m_hull.numChines];
+                m_handle = new Rectangle[m_hull.numChines];
 
                 for (int ii=0; ii<m_hull.numChines; ii++)
                 {
-                    m_handle[ii] = new Button();
                     Rectangle rect = new Rectangle();
-                    rect.Height = 8;
-                    rect.Width = 8;
+                    rect.Height = RECT_SIZE;
+                    rect.Width = RECT_SIZE;
                     rect.Stroke = new SolidColorBrush(Colors.Black); ;
                     rect.StrokeThickness = 1;
-                    Canvas.SetTop(rect, points[ii, 1] - 4);
-                    Canvas.SetLeft(rect, points[ii, 0] - 4);
+                    Canvas.SetTop(rect, points[ii, 1] - RECT_SIZE/2);
+                    Canvas.SetLeft(rect, points[ii, 0] - RECT_SIZE/2);
                     HullCanvas.Children.Add(rect);
+
+                    m_handle[ii] = rect;
                 }
             }
         }
 
-        protected Button[] m_handle;
+        protected Rectangle[] m_handle;
+        protected bool m_Dragging;
+        protected int m_DraggingHandle;
+        protected double m_dragX;
+        protected double m_dragY;
+        const int RECT_SIZE = 8;
+
+        private void HullCanvas_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point loc = e.GetPosition(HullCanvas);
+
+            Console.WriteLine("Preview Mouse Down {0},{1}", loc.X, loc.Y);
+            if (m_handle == null) return;
+
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                if (m_Dragging)
+                {
+                    m_dragX = loc.X;
+                    m_dragY = loc.Y;
+
+                    m_handle[m_DraggingHandle].SetValue(Canvas.TopProperty, m_dragY - RECT_SIZE/2);
+                    m_handle[m_DraggingHandle].SetValue(Canvas.LeftProperty, m_dragX - RECT_SIZE/2);
+
+                    //HullCanvas.Children.Remove(m_handle[m_DraggingHandle]);
+
+                    //Rectangle rect = new Rectangle();
+                    //rect.Height = 8;
+                    //rect.Width = 8;
+                    //rect.Stroke = new SolidColorBrush(Colors.Black); ;
+                    //rect.StrokeThickness = 1;
+                    //Canvas.SetTop(rect, m_dragY - 4);
+                    //Canvas.SetLeft(rect, m_dragX - 4);
+                    //HullCanvas.Children.Add(rect);
+
+                    //m_handle[m_DraggingHandle] = rect;
+                }
+                else
+                {
+                    m_dragX = loc.X;
+                    m_dragY = loc.Y;
+
+                    Console.WriteLine("Checking for MouseOver");
+                    for (int ii=0; ii<m_handle.Length; ii++)
+                    {
+                        Console.WriteLine("Checking {0},{1}",
+                            m_handle[ii].GetValue(Canvas.LeftProperty), m_handle[ii].GetValue(Canvas.TopProperty));
+
+                        if (m_handle[ii].IsMouseOver)
+                        {
+                            m_DraggingHandle = ii;
+                            m_Dragging = true;
+                            Console.WriteLine("Mouse over {0} {1},{2}", ii, m_dragX, m_dragY);
+                            break;
+                        }
+
+                        double x = (double)m_handle[ii].GetValue(Canvas.LeftProperty);
+                        double y = (double)m_handle[ii].GetValue(Canvas.TopProperty);
+                        if (x <= loc.X && x+RECT_SIZE >= loc.X &&
+                            y <= loc.Y && y + RECT_SIZE >= loc.Y)
+                        {
+                            m_DraggingHandle = ii;
+                            m_Dragging = true;
+                            Console.WriteLine("Found {0} {1},{2}", ii, m_dragX, m_dragY);
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (m_Dragging)
+                {
+                    Console.WriteLine("Dropped at {0} {1}", m_dragX, m_dragY);
+                    m_Dragging = false;
+                }
+            }
+
+        }
+
+        private void HullCanvas_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (m_Dragging)
+            {
+                Point loc = e.GetPosition(HullCanvas);
+                m_dragX = loc.X;
+                m_dragY = loc.Y;
+
+                m_handle[m_DraggingHandle].SetValue(Canvas.TopProperty, m_dragY - RECT_SIZE / 2);
+                m_handle[m_DraggingHandle].SetValue(Canvas.LeftProperty, m_dragX - RECT_SIZE / 2);
+
+                Console.WriteLine("Moved {0} to {1},{2}", m_DraggingHandle, loc.X, loc.Y);
+            }
+        }
     }
 }
